3c472eabb917049dfed40233309412ec1c955f76
purini-to <purini-to@example.com>
2015-02-22 18:41:18 +0000

    MySQLの永続化対応 Jenkinsのイメージを軽くするために処理見直し ALMiniumのMySQL永続化対応（他はまだ）

--- alminium/run.sh
+++ alminium/run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-cd alminium
-service mysqld start
+# MySQL?~A??~B??~C??~B??~C~H?~C??~C??~C~G?~B??~C??~B??~C~H?~C?
+mysqlDir="/var/lib/mysql"
+
+if [[ ! -d $mysqlDir/mysql ]]; then
+        echo "?~C~\?~C??~C??~C??~C| ?~A~L?~X?~\??~A~W?~A??~A~D?~A~_?~B~A?~@~AMyy
+SQL?~A??~H~]?~\~_?~L~V?~B~R?~L?~A~D?~A??~A~Y"
+        mysql_install_db
+        service mysqld start
+        sleep 5s
+        mysql < /alminium/config/createdb.sql
+        cd /opt/alminium
+        bundle exec rake db:migrate RAILS_ENV=production
+        bundle exec rake redmine:plugins:migrate RAILS_ENV=production
+        mysql alminium < /alminium/config/init.mysql
+else
+        echo "?~C~^?~B??~C??~C~H?~A~U?~B~L?~A~_MySQL?~B~R使?~T??~A~W?~A??~A~Y"
+        service mysqld start
+fi
+
+cd /opt/alminium
 bundle exec rake tmp:cache:clear
 bundle exec rake tmp:sessions:clear
 service httpd start
--- mysql/run.sh
+++ mysql/run.sh
@@ -3,17 +3,16 @@
 # MySQLのインストールディレクトリ
 mysqlDir="/var/lib/mysql"
 
-if [ -z "$(ls -A $mysqlDir)" ]
-then
+if [[ ! -d $mysqlDir/mysql ]]; then
 	echo "ボリュームが存在しないため、MySQLの初期化を行います"
 	mysql_install_db
-        service mysqld start ;\
-        sleep 5s ;\
+        service mysqld start
+        sleep 5s
         mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO root@\"172.%\" WITH GRANT OPTION; FLUSH PRIVILEGES;"
         mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO admin@\"172.%\" IDENTIFIED BY 'admin' WITH GRANT OPTION; FLUSH PRIVILEGES;"
         mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO admin@\"192.%\" IDENTIFIED BY 'admin' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 else
         echo "マウントされたMySQLを使用します"
-        service mysqld start &
+        service mysqld start
 fi
 tail -F /dev/null

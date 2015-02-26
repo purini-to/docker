ec92f4d8eb05a0b463576af335205279f76908b0
purini-to <purini-to@example.com>
2015-02-26 04:36:32 +0000

    Jenkinsのプラグイン読み込み設定を変更

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -7,11 +7,7 @@ if [ -z "$(ls -A $jenkinsDir)" ]
 then
         echo "ボリュームが存在しないため、Jenkinsの初期化を行います"
         # プラグインのインストール
-	wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins https://updates.jenkins-ci.org/latest/scm-api.hpi
-	wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins https://updates.jenkins-ci.org/latest/git-client.hpi
-	wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins https://updates.jenkins-ci.org/latest/git.hpi
-	wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins https://updates.jenkins-ci.org/latest/ansicolor.hpi
-	wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins https://updates.jenkins-ci.org/latest/postbuild-task.hpi
+        grep -v "^#" /plugin.lst | xargs wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins
 	chown -R jenkins:jenkins /var/lib/jenkins/plugins
 else
         echo "マウントされたJenkinsを使用します"

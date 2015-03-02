551163a67cf0283b7fa695390e42a2cfad53df1a
purini-to <example@com.jp>
2015-03-02 03:41:16 +0000

    Jenkinsのプラグインを追加 テーマを設定

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -8,6 +8,7 @@ then
         echo "ボリュームが存在しないため、Jenkinsの初期化を行います"
         # プラグインのインストール
         grep -v "^#" /plugin.lst | xargs wget -nv -T 60 -t 3 -P /var/lib/jenkins/plugins
+        mv /org.codefirst.SimpleThemeDecorator.xml /var/lib/jenkins/
 	chown -R jenkins:jenkins /var/lib/jenkins/plugins
 else
         echo "マウントされたJenkinsを使用します"

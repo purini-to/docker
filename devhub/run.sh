a4b7bdb10bdd9ddcd55189b25bdc4bc80eab0371
purini-to <purini-to@example.com>
2015-03-03 16:03:45 +0000

    DevHub永続化対応をå®

--- devhub/run.sh
+++ devhub/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [ "$MONGODB_PORT" != '' ]
-then
-        mongodbUrl=`echo $MONGODB_PORT | sed -e "s/tcp/mongodb/"`
-        echo $mongodbUrl
-	export MONGOLAB_URI=$mongodbUrl
+if [ "$TITLE" -eq "" ]; then
+    TITLE="DevHub"
 fi
+
+service mongod start
 cd DevHub
-node app.js -p 3000 -d devhub_db -t DevHub
+node app.js -p 3000 -d devhub_db -t $TITLE
